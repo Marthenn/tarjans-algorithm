4aaf4dd32dea30f177012f59f496088728ac7468
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-07-12 14:03:47 +0700

    feat: file to adj list parser

---
+++ backend/parser.go
@@ -0,0 +1,31 @@
+package main
+
+import(
+	"os"
+	"fmt"
+	"bufio"
+	"strings"
+)
+
+func FileToAdjList(dir string) map[string][]string{
+	adjList := make(map[string][]string)
+
+	file, err := os.Open(dir)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan(){
+		line := scanner.Text()
+		words := strings.Split(line, " ")
+		adjList[words[0]] = append(adjList[words[0]], words[1])
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+
+	return adjList
+}
