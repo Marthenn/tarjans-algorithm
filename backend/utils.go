dcddd0635ed2ae72e541e3687aca68d09ed2ca52
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-07-12 14:47:07 +0700

    feat: make directed graph to undirected

--- backend/utils.go
+++ backend/utils.go
@@ -1,18 +1,25 @@
 package main
 
-import(
-	"os"
-	"fmt"
+import (
 	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
-func AddEdge(adjList map[string][]string, a, b string) map[string][]string{
+func AddEdge(adjList map[string][]string, a, b string) map[string][]string {
+	// check if b is alread in a's list
+	for _, v := range adjList[a] {
+		if v == b {
+			return adjList
+		}
+	}
+
 	adjList[a] = append(adjList[a], b)
 	return adjList
 }
 
-func FileToAdjList(dir string) map[string][]string{
+func FileToAdjList(dir string) map[string][]string {
 	adjList := make(map[string][]string)
 
 	file, err := os.Open(dir)
@@ -22,7 +29,7 @@ func FileToAdjList(dir string) map[string][]string{
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	for scanner.Scan(){
+	for scanner.Scan() {
 		line := scanner.Text()
 		words := strings.Split(line, " ")
 		adjList = AddEdge(adjList, words[0], words[1])
@@ -30,6 +37,16 @@ func FileToAdjList(dir string) map[string][]string{
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
+	}
+
+	return adjList
+}
+
+func MakeUndirected(adjList map[string][]string) map[string][]string {
+	for k, v := range adjList {
+		for _, w := range v {
+			adjList = AddEdge(adjList, w, k)
+		}
 	}
 
 	return adjList
