d2246d4c928dbb556dccdcebe90a1e7d15870701
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-07-13 15:12:08 +0700

    feat: FileToAdjList handler

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/golang-collections/collections/set"
 )
 
 type Message struct {
@@ -14,9 +16,52 @@ type Message struct {
 }
 
 func main() {
+	http.HandleFunc("/file", FileHandler)
 	http.HandleFunc("/addEdge", AddEdgeHandler)
 	port := ":8080"
 	http.ListenAndServe(port, nil)
+}
+
+func FileHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var message Message
+	err = json.Unmarshal(body, &message)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if message.U == "" {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	names_set := set.New()
+	adjList := FileToAdjList(message.U, names_set)
+
+	message.AdjList = adjList
+	message.Names = SetToList(names_set)
+	message.U = ""
+	message.V = ""
+
+	response, err := json.Marshal(message)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
 }
 
 func AddEdgeHandler(w http.ResponseWriter, r *http.Request) {
